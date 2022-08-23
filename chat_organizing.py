abbb7798601f4cc275ec8794182115dbae5bd6d4
Aryan <aryanradadiya18@gmail.com>
2022-08-23 13:25:02 +0530

    updated code

--- chat_organizing.py
+++ chat_organizing.py
@@ -11,11 +11,20 @@ def preprocess(chat):
     df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
     df.rename(columns={'message_date': 'date'}, inplace=True)
 
+    df['only_date'] = df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+    
     users = []
     messages = []
     for message in df['user_message']:
         entry = re.split('([\w\W]+?):\s', message)
-        if entry[1:]:  # user name
+        if entry[1:]:
             users.append(entry[1])
             messages.append(" ".join(entry[2:]))
         else:
@@ -25,14 +34,5 @@ def preprocess(chat):
     df['user'] = users
     df['message'] = messages
     df.drop(columns=['user_message'], inplace=True)
-
-    df['only_date'] = df['date'].dt.date
-    df['year'] = df['date'].dt.year
-    df['month_num'] = df['date'].dt.month
-    df['month'] = df['date'].dt.month_name()
-    df['day'] = df['date'].dt.day
-    df['day_name'] = df['date'].dt.day_name()
-    df['hour'] = df['date'].dt.hour
-    df['minute'] = df['date'].dt.minute
 
     return df
